65bf1560d9a87aa4b186ff5a9791b77e02d46364
Marcos Junior <marcos.jrk@hotmail.com>
2025-10-27 17:33:22 -0300

    Desafio nivel novato
    criado os cadastros das duas cartas em dois conjuntos de variáveis, e exibindo os valores capturados no final da tela.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,11 +5,100 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+  // ###Área para definição das variáveis para armazenar as propriedades das cidades###
 
-  // Área para entrada de dados
+char estado1, estado2;
+char codCarta1[4], codCarta2[4];
+char cidade1[15], cidade2[15];
+int populacao1, populacao2;
+float area1, area2;
+float PIB1, PIB2;
+int turismo1, turismo2;
 
-  // Área para exibição dos dados da cidade
+  //variaveis tem os mesmos nomes, só um numero no final para diferenciar a primeira e segunda carta
+
+  // ###Área para entrada de dados###
+
+printf("***Sistema de cadastro de cartas***\n");
+//banner principal do sistema, irá se repetir durante as telas
+
+//entradas de dados para a primeira carta, como as linhas se mantem na tela só irá pedir as informações especificamente da primera carta
+    printf("Digite os dados da primeira carta:\n");
+    
+        printf("Estado: \n");
+        scanf(" %c", &estado1);
+        
+        printf("Codigo da carta: \n");
+        scanf("%s", codCarta1);
+
+        printf("Nome da cidade: \n");
+        scanf("%s", cidade1);
+        
+        printf("População: \n");
+        scanf("%d", &populacao1);
+        
+        printf("Area em km²: \n");
+        scanf("%f", &area1);
+        
+        printf("PIB: \n");
+        scanf("%f", &PIB1);
+        
+        printf("Numero de pontos turisticos: \n");
+        scanf("%d", &turismo1);
+
+//agora recolhendo os dados para a segunda carta, novamente as informações ficam na tela então as linhas de printf só pedem o dado especifico
+
+    printf("***Sistema de cadastro de cartas***\n");
+//banner principal do sistema, irá se repetir durante as telas
+
+    printf("Digite os dados da segunda carta:\n");
+    
+        printf("Estado: \n");
+        scanf(" %c", &estado2);
+
+        printf("Codigo da carta: \n");
+        scanf("%s", codCarta2);
+
+        printf("Nome da cidade: \n");
+        scanf("%s", cidade2);
+
+       printf("População: \n");
+       scanf("%d", &populacao2);
+       
+       printf("Area em km²: \n");
+       scanf("%f", &area2);
+       
+       printf("PIB: \n");
+       scanf("%f", &PIB2);
+       
+       printf("Numero de pontos turisticos: \n");
+       scanf("%d", &turismo2);
+
+
+  // ###Área para exibição dos dados da cidade###
+
+printf("***Sistema de cadastro de cartas***\n");
+//banner principal do sistema, irá se repetir durante as telas
+
+    printf("Cadastro completo, seguem os dados abaixo. \n");
+
+    printf("Carta 1: \n");
+    printf("Estado: %c\n", estado1);
+    printf("Código da carta: %s\n", codCarta1);
+    printf("Nome da cidade: %s\n", cidade1);
+    printf("População: %d\n", populacao1);
+    printf("Área em Km²: %.3f\n", area1);
+    printf("PIB: %.2f\n", PIB1);
+    printf("Numero de pontos turisticos: %d\n", turismo1);
+
+    printf("Carta 2: \n");
+    printf("Estado: %c\n", estado2);
+    printf("Código da carta: %s\n", codCarta2);
+    printf("Nome da cidade: %s\n", cidade2);
+    printf("População: %d\n", populacao2);
+    printf("Área em Km²: %.3f\n", area2);
+    printf("PIB: %.2f\n", PIB2);
+    printf("Numero de pontos turisticos: %d\n", turismo2);
 
 return 0;
 } 
